b6f7dc2bd3f11784fbaa771c7724c51b68259626
John Hamelink <john@johnhamelink.com>
2016-09-14 15:53:48 +0100

    Add documentation deps to test environment

--- mix.exs
+++ mix.exs
@@ -35,9 +35,9 @@ defmodule ICalendar.Mixfile do
       {:mix_test_watch, ">= 0.0.0", only: :dev},
 
       # Markdown processor
-      {:earmark, ">= 0.0.0", only: :dev},
+      {:earmark, ">= 0.0.0", only: [:dev, :test]},
       # Documentation generator
-      {:ex_doc, ">= 0.0.0", only: :dev},
+      {:ex_doc, ">= 0.0.0", only: [:dev, :test]},
     ]
   end
 end
