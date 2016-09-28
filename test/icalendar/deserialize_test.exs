13672d9c581c7788f9ec6fe255b829532807fec0
John Hamelink <john@johnhamelink.com>
2016-09-28 19:02:04 +0100

    Add test to prove RRULE works inside an iCal string

--- test/icalendar/deserialize_test.exs
+++ test/icalendar/deserialize_test.exs
@@ -35,6 +35,22 @@ defmodule ICalendar.DeserializeTest do
       assert event.dtstart.time_zone == "America/Chicago"
       assert event.dtend.time_zone == "America/Chicago"
     end
+
+    test "with RRULE" do
+      ics = """
+      BEGIN:VEVENT
+      RRULE:FREQ=DAILY;UNTIL=22221224T084500Z;BYMONTHDAY=1,3,5;BYDAY=TU,FR;BYMONTH=4
+      END:VEVENT
+      """
+
+      event = ICalendar.from_ics(ics)
+      assert event.rrule.by_day == [:tuesday, :friday]
+      assert event.rrule.by_month == [:april]
+      assert event.rrule.by_month_day == [1, 3, 5]
+      assert event.rrule.frequency == :daily
+      assert event.rrule.until == Timex.to_datetime({{2222, 12, 24}, {8, 45, 0}}, "Etc/UTC")
+    end
+
   end
 
 end
