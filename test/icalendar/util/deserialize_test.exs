79b9159be5cfb2584399f2ca37cc08d744449289
Ryan Winchester <ryan@sevenshores.co>
2021-01-16 13:35:42 -0800

    Remove redundant skipped test

--- test/icalendar/util/deserialize_test.exs
+++ test/icalendar/util/deserialize_test.exs
@@ -74,36 +74,6 @@ defmodule ICalendar.Util.DeserializeTest do
     assert %Event{} = event
   end
 
-  @tag :skip
-  test "Include ORGANIZER and ATTENDEEs in event" do
-    event =
-      """
-      BEGIN:VEVENT
-      DTSTART:20190711T130000Z
-      DTEND:20190711T150000Z
-      DTSTAMP:20190719T195201Z
-      ORGANIZER;CN=paul@clockk.com:mailto:paul@clockk.com
-      UID:7E212264-C604-4071-892B-E0A28048F1BA
-      ATTENDEE;CUTYPE=INDIVIDUAL;ROLE=REQ-PARTICIPANT;PARTSTAT=ACCEPTED;CN=eric@clockk.com;X-NUM-GUESTS=0:mailto:eric@clockk.com
-      ATTENDEE;CUTYPE=INDIVIDUAL;ROLE=REQ-PARTICIPANT;PARTSTAT=ACCEPTED;CN=paul@clockk.com;X-NUM-GUESTS=0:mailto:paul@clockk.com
-      ATTENDEE;CUTYPE=INDIVIDUAL;ROLE=REQ-PARTICIPANT;PARTSTAT=ACCEPTED;CN=James SM;X-NUM-GUESTS=0:mailto:james@clockk.com
-      CREATED:20190709T192336Z
-      DESCRIPTION:
-      LAST-MODIFIED:20190711T130843Z
-      LOCATION:In-person at Volta and https://zoom.us/j/12345678
-      SEQUENCE:0
-      STATUS:CONFIRMED
-      SUMMARY:Design session
-      END:VEVENT
-      """
-      |> String.trim()
-      |> String.split("\n")
-      |> Deserialize.build_event()
-
-    assert %Event{} = event
-    assert event.organizer == "mailto:paul@clockk.com"
-  end
-
   test "Include ORGANIZER w/ params and ATTENDEEs in event" do
     event =
       """
